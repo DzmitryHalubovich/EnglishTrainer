a0e9c669a428b532ba5fa13c253d7fc629397199
DzmitryHalubovich <buxarik493@gmail.com>
2023-04-18 12:57:09 +0300

    add http client into controller

--- src/EnglishTrainer.Web/Configuration/ConfigureCoreService.cs
+++ src/EnglishTrainer.Web/Configuration/ConfigureCoreService.cs
@@ -4,12 +4,13 @@ using EnglishTrainer.ApplicationCore.Models;
 using EnglishTrainer.Infrastructure.Data;
 using EnglishTrainer.Services.Implementations;
 using EnglishTrainer.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EnglishTrainer.Services
 {
     public static class ConfigureCoreService
     {
-        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped(typeof(IRepository<>), typeof(EFTrainerRepository<>));
             services.AddScoped(typeof(IVerbViewModelService), typeof(VerbViewModelService));
@@ -19,7 +20,12 @@ namespace EnglishTrainer.Services
             services.AddScoped(typeof(IExampleViewModelService),typeof(ExampleViewModelService));
             services.AddScoped(typeof(ITokenService), typeof(TokenService));
             services.AddScoped(typeof(IUserService), typeof(UserService));
-            services.AddScoped(typeof(IPictureService), typeof(PictureService));
+            services.AddHttpClient();
+            services.AddHttpClient("meta", c =>
+            {
+                c.BaseAddress = new Uri(configuration.GetValue<string>("MetaAPI"));
+            });
+
 
             return services;
         }
--- src/EnglishTrainer.Web/Controllers/Api/DictionaryController.cs
+++ src/EnglishTrainer.Web/Controllers/Api/DictionaryController.cs
@@ -1,8 +1,10 @@
 ﻿using EnglishTrainer.ApplicationCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace EnglishTrainer.Web.Controllers.Api
 {
@@ -29,28 +31,43 @@ namespace EnglishTrainer.Web.Controllers.Api
         [Route("dictionaryPartial")]
         public async Task<IActionResult> DictionaryPartial([FromServices] IHttpClientFactory _clientFactory)
         {
-            var dictionary = new List<WordViewModel>();
+            //var dictionary = new List<WordViewModel>();
 
             ///////////////////////////////////////
 
-            var request = new HttpRequestMessage(HttpMethod.Get, "");
+            //var request = new HttpRequestMessage(HttpMethod.Get, "");
 
-            var client = _clientFactory.CreateClient();
+            //var client = _clientFactory.CreateClient();
 
-            HttpResponseMessage response = await client.SendAsync(request);
+            //HttpResponseMessage response = await client.SendAsync(request);
 
             /////////////////////
+            ///
 
-            using (var httpClient = new HttpClient())
+            var client = _clientFactory.CreateClient("meta");
+            string errorString;
+
+            try
             {
-                using (var response = await httpClient.GetAsync("http://localhost:5268/api/dictionary"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    dictionary = JsonConvert.DeserializeObject<List<WordViewModel>>(apiResponse);
-                }
+               var dictionary = await client.GetFromJsonAsync<List<WordViewModel>>("dictionary");
+                errorString = null;
+                return PartialView("_DictionaryPartial", dictionary);
+            }
+            catch (Exception ex)
+            {
+                errorString = $"There was an error getting our forecast: {ex.Message}";
             }
 
-            return PartialView("_DictionaryPartial", dictionary);
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var response = await httpClient.GetAsync("http://localhost:5268/api/dictionary"))
+            //    {
+            //        string apiResponse = await response.Content.ReadAsStringAsync();
+            //        dictionary = JsonConvert.DeserializeObject<List<WordViewModel>>(apiResponse);
+            //    }
+            //}
+
+            return View();
         }
     }
 }
